c973ef2ea57fabd2307a00fc9e16abfc783ae5a5
kathrine <1795206343@qq.com>
2024-06-04 22:08:20 +0800

    message 17:change models for multiple users

--- lists/tests.py
+++ lists/tests.py
@@ -3,10 +3,23 @@ from django.urls import resolve
 from lists.views import  home_page
 from django.http import HttpRequest
 from django.template.loader import  render_to_string
-from lists.models import Item
+from lists.models import Item,List
 
 class ListViewTest(TestCase):
+    def test_displays_all_list_items(self):
+        list_user = List.objects.create()
+        Item.objects.create(text='itemey 1', list=list_user)
+        Item.objects.create(text='itemey 2', list=list_user)
 
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirects_after_POST(self):
+        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        self.assertRedirects(response, '/lists/the-new-page/')
 
     def test_user_list_template(self):
         response = self.client.get('/lists/the-new-page/')
@@ -36,18 +49,6 @@ class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
-
-    def test_can_save_a_POST_request(self):
-        response = self.client.post('/', data={'item_text': 'A new list item'})
-
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, 'A new list item')
-
-    def test_redirects_after_POST(self):
-        response = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/the-new-page/')
 
 
     def test_only_saves_items_when_necessary(self):
--- lists/views.py
+++ lists/views.py
@@ -1,15 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from lists.models import Item
+from lists.models import Item,List
 
 
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-new-page/')
     return render(request, 'home.html')
 
 
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
+
+
+def new_list(request):
+    list_user = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_user)
+    return redirect('/lists/the-new-page/')
